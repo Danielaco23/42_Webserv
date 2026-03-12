fc016a0220ede31ffb9be4ba1389342f9ded43ca
Owmarqui <fire20leo@gmail.com>
2026-03-12 18:00:23 +0100

    Estructura basica creo

---
+++ config/Config.cpp
@@ -0,0 +1,24 @@
+#include "Config.hpp"
+
+LocationConfig::LocationConfig()
+{
+    path = "/";
+    root = "";
+    autoindex = false;
+    cgi_extension = "";
+    cgi_path = "";
+    cgi_path = "";
+    upload_path = "";
+}
+
+ServerConfig::ServerConfig()
+{
+    host = "0.0.0.0";
+    port = 80;
+    root = "";
+    client_max_body_size = 1000000;
+}
+
+Config::Config()
+{
+}
