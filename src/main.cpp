f96c8557c45e19005bf3ba229b4ada6746a452d9
Daniel Gomez-Llusa <dgomez-l@c2r19s2.42madrid.com>
2026-03-14 15:28:05 +0100

    Examples of function documentation that I'd like to implement project-wide

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 
 #include "webserv.hpp"
 
+/**
+ * @brief This function prints a given string to the standard output
+ * @param printable String to be printed to the std::cout
+ */
 void	print_cout(std::string printable)
 {
 	std::cout << printable << std::endl;
 }
 
+/**
+ * @brief This function prints a given string, in red, to the error output
+ * @param printable String to be printed to the std::cerr
+ */
 void	print_cerr(std::string printable)
 {
 	const std::string color_R = "\033[0;31m";
@@ -13,6 +21,12 @@ void	print_cerr(std::string printable)
 	std::cerr << color_R << printable << color_NC << std::endl;
 }
 
+/**
+ * @brief The main function of the project, kickstarts the execution and begins the program.
+ * @param argc Number of arguments, including the executable name.
+ * @param argv Constant array of C strings,
+ * containing the arguments in order of introduction, with position 0 being the executable mane.
+ */
 int main(int argc, char const *argv[])
 {
 	if (argc != 2)
